865e6e67b8b9c30f1741d9ed9c4a25a16ec32a34
Peter Safwat <peter-safwat@ibm.com>
2023-12-16 17:34:14 +0200

    add redirect logic for authentication

--- middleware.tsx
+++ middleware.tsx
@@ -1,12 +1,16 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { NextResponse, type NextRequest } from 'next/server';
-import { env } from './lib/env.server';
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+import { env } from "./lib/env.server";
 
 export async function middleware(request: NextRequest) {
   assert_origin: {
-    const origin = new URL(request.headers.get("referer") ?? env.NEXT_PUBLIC_ORIGIN).origin;
+    const origin = new URL(
+      request.headers.get("referer") ?? env.NEXT_PUBLIC_ORIGIN
+    ).origin;
     if (origin !== env.NEXT_PUBLIC_ORIGIN) {
-      throw new Error(`You are trying to access the application from ${origin}, but the origin defined in the NEXT_PUBLIC_ORIGIN environment variable is ${env.NEXT_PUBLIC_ORIGIN}.\nYou must access the application from ${env.NEXT_PUBLIC_ORIGIN}, because some integrations will not work otherwise.`);
+      throw new Error(
+        `You are trying to access the application from ${origin}, but the origin defined in the NEXT_PUBLIC_ORIGIN environment variable is ${env.NEXT_PUBLIC_ORIGIN}.\nYou must access the application from ${env.NEXT_PUBLIC_ORIGIN}, because some integrations will not work otherwise.`
+      );
     }
   }
 
@@ -44,7 +48,7 @@ export async function middleware(request: NextRequest) {
         remove(name: string, options: CookieOptions) {
           request.cookies.set({
             name,
-            value: '',
+            value: "",
             ...options,
           });
           response = NextResponse.next({
@@ -54,7 +58,7 @@ export async function middleware(request: NextRequest) {
           });
           response.cookies.set({
             name,
-            value: '',
+            value: "",
             ...options,
           });
         },
@@ -64,6 +68,27 @@ export async function middleware(request: NextRequest) {
 
   await supabase.auth.getSession();
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  console.log(user);
+
+  // if user is signed in and the current path is / redirect the user to /account
+  if (user && request.nextUrl.pathname === "/admin/public/login") {
+    console.log("redirect");
+    return NextResponse.redirect(
+      new URL("/admin/internal/dashboard", request.url)
+    );
+  }
+
+  // if user is not signed in and the current path is not / redirect the user to /
+  if (!user && request.nextUrl.pathname === "/admin/internal/dashboard") {
+    return NextResponse.redirect(new URL("/admin/public/login", request.url));
+  }
+
   return response;
 }
 
+export const config = {
+  matcher: ["/admin/internal/dashboard", "/admin/public/login"],
+};
