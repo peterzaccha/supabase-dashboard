639ada83e1454a114566f4812e52e84941ad8cbf
Peter Safwat <peter-safwat@ibm.com>
2023-12-16 17:36:10 +0200

    Logout link

---
+++ utility-components/LogoutLink.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { supabaseForClientComponent } from "@/lib/supabase.client";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+export default function LogoutLink() {
+  const supabase = supabaseForClientComponent;
+
+  const router = useRouter();
+  async function logout() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      toast.error(error.message);
+    } else {
+      router.push("/");
+    }
+  }
+
+  return (
+    <Link href="#" onClick={logout}>
+      Logout
+    </Link>
+  );
+}
